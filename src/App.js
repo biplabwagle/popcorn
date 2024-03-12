772c6ced7ba38d0335967220dfce3d75a5f5156e
biplab-w <waglebip@gmail.com>
2024-03-12 12:01:54 -0500

    Bugs fixes

--- src/App.js
+++ src/App.js
@@ -1,51 +1,5 @@
 import { useEffect, useState } from "react";
 const KEY = "e7e8e491";
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -63,7 +17,6 @@ const Logo = () => {
 };
 
 const Search = ({ searchQuery, setSearchQuery }) => {
-  // const [query, setQuery] = useState("");
   return (
     <input
       className="search"
@@ -219,12 +172,17 @@ const ErrorMessage = ({ message }) => {
   );
 };
 
+function SelectedMovie({ selectedId }) {
+  return <div className="details">{selectedId}</div>;
+}
+
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
+  const [selectedId, setSelectedId] = useState(null);
 
   //http://www.omdbapi.com/?i=tt3896198&apikey=e7e8e491
   //fetch function
@@ -233,6 +191,7 @@ export default function App() {
     async function fetchMovies() {
       try {
         setIsLoading(true);
+        setError("");
         if (searchQuery.length >= 1) {
           const res = await fetch(
             `https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${searchQuery}`
@@ -244,8 +203,11 @@ export default function App() {
 
           const data = await res.json();
 
-          if (data.Response === "False") throw new Error("Movie not found");
-          // console.log(data);
+          console.log(`data Error: ${data.Error} & response: ${data.Response}`);
+          if (data.Error && data.Response === "False") {
+            throw new Error(data.Error || "Please type in a different query");
+          }
+
           setMovies(data.Search);
         }
       } catch (error) {
@@ -254,6 +216,11 @@ export default function App() {
       } finally {
         setIsLoading(false);
       }
+    }
+
+    if (searchQuery.length < 3) {
+      setMovies([]);
+      return;
     }
     fetchMovies();
   }, [searchQuery]);
@@ -275,8 +242,14 @@ export default function App() {
         </Box>
         <Box>
           <>
-            <Summary watched={watched} />
-            <WatchedMovieList watched={watched} />
+            {selectedId ? (
+              <SelectedMovie selectedId={selectedId} />
+            ) : (
+              <>
+                <Summary watched={watched} />
+                <WatchedMovieList watched={watched} />
+              </>
+            )}
           </>
         </Box>
       </Main>
